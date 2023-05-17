1b9ae32a49675914c4603bdea7391d91e8f23419
15rhema <andrewmbjorklund@gmail.com>
2023-05-17 11:27:51 -0700

    try again

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
-var path = require("path");
+//var path = require("path");
 const nextConfig = {
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, "../../"),
-    outputFileTracingExcludes: {
-      "*": ["node_modules/canvas"]
-    }
-  }
+  // experimental: {
+  //   outputFileTracingRoot: path.join(__dirname, "../../"),
+  //   outputFileTracingExcludes: {
+  //     "*": ["node_modules/canvas"]
+  //   }
+  // }
 };
 
 module.exports = nextConfig;
