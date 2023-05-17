158427b761f9345f46be3c725b49824cdf5b2965
15rhema <andrewmbjorklund@gmail.com>
2023-05-17 11:18:37 -0700

    try 2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
+var path = require("path");
 const nextConfig = {
   experimental: {
-    //outputFileTracingRoot: path.join(__dirname, "../../"),
+    outputFileTracingRoot: path.join(__dirname, "../../"),
     outputFileTracingExcludes: {
       "*": ["node_modules/canvas"]
     }
